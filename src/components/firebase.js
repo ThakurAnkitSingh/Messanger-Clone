d3868f0b9009079c990fd11f714a0fead3ff07f8
ThakurAnkitSingh <akashthakur70423@gmail.com>
2023-01-12 22:17:18 +0530

    first commit

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,12 +1,8 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-export const auth = firebase.initializeApp({
-    apiKey: "AIzaSyDCnY2XOM_viKx1FgcIxvmnu6bU41Mq6B8",
-    authDomain: "unichat-messanger-8804b.firebaseapp.com",
-    projectId: "unichat-messanger-8804b",
-    storageBucket: "unichat-messanger-8804b.appspot.com",
-    messagingSenderId: "803221858205",
-    appId: "1:803221858205:web:a373efc1d0003b17ff3174"
-}).auth();
+
+
+// export const auth = firebase.initializeApp(FirbaseConfig).auth();
+//  FirebaseConfig comes here 
 
